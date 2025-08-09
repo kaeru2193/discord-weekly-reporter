1da11bbcd7214d566a61563baa009d28ef5a64b0
kaeru2193 <kaeru2193@gmail.com>
2025-08-09 11:16:55 +0900

    updated: report generating.

--- bot.ts
+++ bot.ts
@@ -1,4 +1,4 @@
-import { GatewayIntentBits, Client, Events, Message } from 'discord.js'
+import { GatewayIntentBits, Client, Events, Message, SendableChannels } from 'discord.js'
 import dotenv from 'dotenv'
 import fs from "fs"
 import cron from "node-cron"
@@ -9,8 +9,12 @@ dotenv.config()
 
 const TOKEN = process.env.BOT_TOKEN
 const TITLE = process.env.TITLE
+const TITLE_ENGLISH = process.env.TITLE_ENGLISH
 const GUILD = process.env.GUILD
 const CHANNEL = process.env.CHANNEL
+
+const PROMPT = fs.readFileSync("./prompt.txt", "utf8")
+const PROMPT_ENGLISH = fs.readFileSync("./prompt_english.txt", "utf8")
 
 if (!TITLE) { throw Error("title is not defined.")}
 if (!GUILD || !CHANNEL) { throw Error("guild or channel is not defined.")}
@@ -31,7 +35,7 @@ client.on(Events.MessageCreate, async (message: Message) => {
     recordMessage(message)
 })
 
-cron.schedule("0 20 * * 0", async () => { //毎週金曜20:00
+cron.schedule("0 20 * * 0", async () => { //毎週日曜20:00
     await writeReport()
 })
 
@@ -63,9 +67,7 @@ const recordMessage = (message: Message) => {
     fs.writeFileSync("./log.json", JSON.stringify(json))
 }
 
-const formatLog = (start: Date) => {
-    const data = fs.readFileSync("./log.json", "utf-8")
-    const json: Log = JSON.parse(data)
+const formatLog = (json: Log, start: Date) => {
     const days = ["日", "月", "火", "水", "木", "金", "土"]
 
     const filtered = json.map(c => {
@@ -84,26 +86,71 @@ const formatLog = (start: Date) => {
     return channels.join("\n\n")
 }
 
+const nameUpdate = async (log: Log) => {
+    return await Promise.all(log.map(async (c) => {
+        let channelName = ""
+        
+        try {
+            const channel = await client.channels.fetch(c.id) //discordから最新のチャンネル情報を取得
+
+            channelName = (() => { //無名関数をその場で実行（場合分けを楽にするため）
+                if (!channel) { return c.name }
+                if (channel.isDMBased()) { return c.name } //名前が取得できないときは現行の名前を返す
+                return channel.name
+            })()
+        } catch (e) {
+            channelName = c.name
+        }
+
+        c.name = channelName
+        return c
+    }))
+}
+
 const writeReport = async () => {
+    const data = fs.readFileSync("./log.json", "utf-8")
+    const json: Log = JSON.parse(data)
+
+    const updated: Log = await nameUpdate(json) //チャンネル名の更新処理
+    fs.writeFileSync("./log.json", JSON.stringify(updated))
+
     const nowDate = new Date()
     const startDate = new Date()
     startDate.setDate(startDate.getDate() - 7) //7日戻す
 
-    fs.writeFileSync("./formatLog.txt", formatLog(startDate))
+    fs.writeFileSync("./formatLog.txt", formatLog(updated, startDate))
     
     console.log("執筆中…")
-    const report = await GenResponse()
-    const title = `## ${TITLE} ${startDate.toLocaleDateString()}～${nowDate.toLocaleDateString()}`
 
-    const channel = client.channels.cache.get(CHANNEL)
+    const channel = client.channels.cache.get(CHANNEL) //送信するチャンネル
     if (!channel) { return } //存在しなければ終了
     if (!channel.isSendable()) { return }
 
+    const report = await GenResponse(PROMPT)
+    fs.writeFileSync("./output.txt", report)
+
+    const title = `## ${TITLE} ${startDate.toLocaleDateString()}～${nowDate.toLocaleDateString()}`
     const content = title + `\n\n` + report
+    await sendReport(content, channel)
+
+    const reportEnglish = await GenResponse(PROMPT_ENGLISH, "./output.txt") //英語版を執筆
+    fs.writeFileSync("./output_english.txt", reportEnglish)
+
+    const messageFetch = (await channel.messages.fetch({limit: 1})) //最後のメッセージ（botの日本語版レポートを取得）
+    const latestMessage = Array.from(messageFetch)[0][1] //最後のメッセージ本体
+    const englishThread = await latestMessage.startThread({ //そこにスレッドを作成
+        name: `${TITLE_ENGLISH} (English Version)`,
+        autoArchiveDuration: 60
+    })
+    
+    const titleEnglish = `## ${TITLE_ENGLISH} ${startDate.toLocaleDateString()}～${nowDate.toLocaleDateString()}`
+    const contentEnglish = titleEnglish + `\n\n` + reportEnglish
+    await sendReport(contentEnglish, englishThread)
+}
+
+const sendReport = async (content: string, channel: SendableChannels) => {
     const chunkSize = 1900
     for (let pos = 0; pos < content.length; pos += chunkSize) {
         await channel.send(content.slice(pos, pos + chunkSize))
     }
-
-    fs.writeFileSync("./output.txt", report)
 }
--- gemini.ts
+++ gemini.ts
@@ -7,22 +7,30 @@ const MODEL = process.env.MODEL
 if (!APIKEY) { throw Error("API KEY is not defined.") }
 if (!MODEL) { throw Error("MODEL is not defined.") }
 
-const PROMPT = fs.readFileSync("./prompt.txt", "utf8")
-
 const genAI = new GoogleGenAI({ apiKey: APIKEY })
 
-export const GenResponse = async () => {
+export const GenResponse = async (prompt: string, refer?: string) => {
     const logFile = await genAI.files.upload({
         file: "./formatLog.txt"
     })
     if (!logFile.uri || !logFile.mimeType) { throw Error("this file has not been uploaded.") }
 
+    let referFile = null
+    if (refer) {
+        referFile = await genAI.files.upload({
+            file: refer
+        })
+        if (!referFile.uri || !referFile.mimeType) { throw Error("this file has not been uploaded.") }
+    }
+
     const res = await genAI.models.generateContent({
         model: MODEL,
         contents: createUserContent([
             createPartFromUri(logFile.uri, logFile.mimeType),
-            "\n\n",
-            PROMPT
+            ...(referFile
+                ? ["\n\n", createPartFromUri(referFile.uri as string, referFile.mimeType as string), "\n\n"]
+                : ["\n\n"]),
+            prompt
         ])
     })
 
